efd8c41efe8096e4c426c778b1a5d4f8c1e6ee87
JayathriRanasinghe <e18283@eng.pdn.ac.lk>
2023-06-16 02:11:02 +0530

    updated entering course via admin dashboard

--- src/components/AdminDashboard.js
+++ src/components/AdminDashboard.js
@@ -33,41 +33,61 @@ const handleChatBotClick = () => {
 
 
 const CourseForm = () => {
+  const [courseBatch,setBatch] = useState('');
   const [courseCode, setCourseCode] = useState('');
   const [courseName, setCourseName] = useState('');
   const [courseCoordinator, setCourseCoordinator] = useState('');
   const [courseInstructors, setCourseInstructors] = useState('');
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
 
-    // Create an object with the course data
-    const courseData = {
-      courseCode,
-      courseName,
-      courseCoordinator,
-      courseInstructors
-    };
-
-    // Send the data to your database or API for further processing
-    // Example: sendCourseDataToDatabase(courseData);
-
-    // Reset the form
-    setCourseCode('');
-    setCourseName('');
-    setCourseCoordinator('');
-    setCourseInstructors('');
+    try {
+      const db = firebase.firestore();
+  
+      // Create an object with the course data
+      const courseData = {
+        courseBatch,
+        courseCode,
+        courseName,
+        courseCoordinator,
+        courseInstructors
+      };
+  
+      // Send the course data to Firestore
+      await db.collection('courses').add(courseData);
+  
+      // Reset the form
+      setBatch('');
+      setCourseCode('');
+      setCourseName('');
+      setCourseCoordinator('');
+      setCourseInstructors('');
+  
+      // Display a success message or perform any other necessary actions
+    } catch (error) {
+      // Handle the error
+    }
   };
 
   return (
     <div>
     <NavBar/>
-    <Row style={{ marginTop: '30px' }}>
-        <Col xs={12} md={3} lg={4} className="mb-4"><ChartComponent/></Col>
+    <Row className="justify-content-between" style={{ marginTop: '30px' }}>
+        <Col xs={12} md={3} lg={4} className="mb-4"><ChartComponent type={1}/></Col>
+        <Col xs={12} md={3} lg={4} className="mb-4"><ChartComponent type={2}/></Col>
         <Col xs={12} md={3} lg={4} className="mb-4">
             <Card className="px-4 py-3" style={{ width: '25rem' }}>
                 <Card.Body>
                         <Form onSubmit={handleSubmit}>
+                    <Form.Group controlId="courseBatch">
+                      <Form.Label>Batch:</Form.Label>
+                      <Form.Control
+                      type="text"
+                      value={courseBatch}
+                      onChange={(event) => setBatch(event.target.value)}
+                    />
+                    </Form.Group>
                     <Form.Group controlId="courseCode">
                         <Form.Label>Course Code:</Form.Label>
                         <Form.Control
@@ -111,6 +131,8 @@ const CourseForm = () => {
                 </Card.Body>
             </Card>
         </Col>
+        
+        
     </Row>
     
     
